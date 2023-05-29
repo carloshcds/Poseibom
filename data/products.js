629227d6ce2b9a32481cb9ced73ccddc82965ba0
Carloshcds <Carloshcds13@gmail.com>
2023-05-29 09:36:12 -0300

    Poseibom

---
+++ data/products.js
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Tilápia Viva',
+        image: "imgtilapiaviva.png",
+        price: 11.99
+
+    },
+    {
+        id: 2,
+        name: 'Filé de Tilápia Congelada',
+        image: "image-6.png",
+        price: 24.99
+
+    },
+    {
+        id: 3,
+        name: 'Filé de Tilápia Fresca',
+        image: "image-5.png",
+        price: 18.99
+    }
+]
